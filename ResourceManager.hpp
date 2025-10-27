3a56385485f54de146a407f0f036f28bf6265aa9
Marek Polewski <marek.polewski.stud@pw.edu.pl>
2025-10-27 22:31:45 +0100

    Lab2 Marek Polewski

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,40 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    Resource* resource;
+
+public:
+    ResourceManager() : resource(new Resource()) {}
+    
+    ~ResourceManager() { delete resource; }
+
+    ResourceManager(const ResourceManager& other)
+        : resource(new Resource(*other.resource)) {}
+
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        if (this != &other)
+        {
+            delete resource;
+            resource = new Resource(*other.resource);
+        }
+        return *this;
+    }
+
+    ResourceManager(ResourceManager&& other) : resource(other.resource)
+    {
+        other.resource = nullptr;
+    }
+
+    ResourceManager& operator=(ResourceManager&& other)    {
+        if (this != &other)
+        {
+            delete resource;
+            resource = other.resource;
+            other.resource = nullptr;
+        }
+        return *this;
+    }
+
+    double get() { return resource->get(); }
 };
